e8e4ddb8229bd5d522031477e16fd4d90a66165c
Aryan Sharma <52833999+aryxns@users.noreply.github.com>
2020-11-11 02:02:19 +0530

    Add files via upload

---
+++ login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:substack/MyHomePage.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("BimaPe")), body: Body());
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String name;
+  TextEditingController controller = new TextEditingController();
+
+  void click() {
+    this.name = controller.text;
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => MyHomePage(this.name)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+              controller: this.controller,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.message),
+                labelText: "Type Name: ",
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.login),
+                  splashColor: Colors.blue,
+                  tooltip: "Post message",
+                  onPressed: this.click,
+                ),
+              )),
+        ));
+  }
+}
